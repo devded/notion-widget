b334e134dc1faed3f60e57249421991f43a209a6
Dedar <thededar@gmail.com>
2023-07-01 02:03:55 +0600

    Remove unsed code

---
+++ expense-chart/main.js
@@ -0,0 +1,67 @@
+function getQueryParameterValues() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var parameterValues = {};
+  urlParams.forEach(function (value, key) {
+    parameterValues[key] = value;
+  });
+  return parameterValues;
+}
+
+var parameterValues = getQueryParameterValues();
+
+console.log("Query parameter values:", parameterValues);
+console.log(JSON.parse(parameterValues["graphData"]));
+
+var graphData = JSON.parse(parameterValues["graphData"]);
+
+var xAxisData = [];
+var depositeData = [];
+var withdrawData = [];
+
+graphData.forEach(function (data) {
+  xAxisData.push(data.month);
+  depositeData.push(data.deposite);
+  withdrawData.push(data.withdraw);
+});
+
+var option = {
+  tooltip: {
+    trigger: "axis",
+  },
+  legend: {
+    data: ["Deposite", "Withdraw"],
+  },
+  grid: {
+    left: "3%",
+    right: "4%",
+    bottom: "3%",
+    containLabel: true,
+  },
+  xAxis: {
+    type: "category",
+    boundaryGap: false,
+    data: xAxisData,
+  },
+  yAxis: {
+    type: "value",
+  },
+  series: [
+    {
+      name: "Deposite",
+      type: "line",
+      data: depositeData,
+      smooth: true,
+    },
+    {
+      name: "Withdraw",
+      type: "line",
+      data: withdrawData,
+      smooth: true,
+    },
+  ],
+};
+
+// ------------------------------ ECharts Config Start ------------------------------
+var myChart = echarts.init(document.getElementById("main"));
+myChart.setOption(option);
+// ------------------------------ ECharts Config End ------------------------------
